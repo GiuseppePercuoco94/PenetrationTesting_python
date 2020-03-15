6f4521f918ecb42c1be79b9e24ea501822d1aa33
GiuseppePercuoco94 <giuseppepercuoco94@gmail.com>
2020-03-15 17:59:05 +0100

    add client

--- Server_socket/server.py
+++ Server_socket/server.py
@@ -9,16 +9,18 @@ import socket
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #retrieve the IP 
-host = socket.gethostbyname()
+host = socket.gethostname()
+print('ip server: ' +str(host)) 
 port = 8888
 
 server_sock.bind((host,port))
 #backlog= number of connection acceppted
-server_sock.listen(backlog=3)
+server_sock.listen(3)
 
 while True:
     client_sock, address = server_sock.accept()
-    print('connection established from ' + str(address))
+    print('connection established from address' + str(address[0])+ 'and port: ' +str(address[1]))
     message = 'Thank you for connecting'
+    client_sock.send(message.encode('utf-8'))
     client_sock.close()
 
